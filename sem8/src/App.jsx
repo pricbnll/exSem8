dd7ad79fa9ddc7358a8ec5b5f387b5123431c958
pricbnll <pricbnll@gmail.com>
2024-07-18 21:16:23 -0300

    feat: Base para autenticação do Yan

--- sem8/src/App.jsx
+++ sem8/src/App.jsx
@@ -1,21 +1,54 @@
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
-import './App.css'
-import Signin from './pages/Singin/Singin'
-import Signup from './/pages/Singup/Signup'
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import Signin from "./pages/Singin/Singin";
+import Signup from "./pages/Singup/Signup";
+import Home from "./pages/Home/Home";
 
 function App() {
+  const isAuthenticated = true;
+  // se true vai para a pagina Login, se false vai para Home
+
+  function loginRedirect(componente) {
+    if (isAuthenticated) {
+      return <Navigate to="/" replace />;
+    }
+
+    return componente;
+  }
 
   return (
     <>
-      <Router> {/* switch */}
+      <Router>
+        {" "}
+        {/* switch */}
         <Routes>
-          <Route path='/' Component={Signin} />
-          <Route path='/login' Component={Signin} />
-          <Route path='/cadastro' Component={Signup} />
+          <Route path="/login" element={loginRedirect(<Signin />)} />
+          <Route path="/cadastro" element={loginRedirect(<Signup />)} />
+
+
+          {/* Se quiser uma rota que não precisa de autenticação coloque aqui a rota */}
+
+          {/* Ternário para redirecionar rota */}
+          {isAuthenticated ? (
+          //Se quiser uma rota autenticada coloque aqui a rota 
+            <>
+              <Route path="/" Component={Home} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          )}
+
+          <Route path="/login" Component={Signin} />
+          <Route path="/cadastro" Component={Signup} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
